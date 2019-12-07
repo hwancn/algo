d6e1fbfca1830c6ecc3d610292a95a806308fb7b
Hwan <hwancd@gmail.com>
2019-12-07 18:21:56 +0800

    69 平方根--二分法

--- leetcode/69-sqrt/69_sqrt.py
+++ leetcode/69-sqrt/69_sqrt.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+# 求平方根
+# 原题：https://leetcode-cn.com/problems/sqrtx/
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        target = x
+        left, right = 1, x
+        while left < right:
+            mid = left + (right - left + 1) // 2
+            if mid * mid > target:
+                right = mid - 1
+            else:
+                left = mid
+        return left
